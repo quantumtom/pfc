c414d1bead4b8cda90222dddf61018d66d6b7c49
quantumtom <cornyn@gmail.com>
2018-03-13 11:08:44 -0700

    Add inital script scaffold.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+import Markup from 'markup/raw.html';
+
 /**
  * Application entry point
  */
@@ -8,3 +10,22 @@ import 'styles/index.scss';
 // ================================
 // START YOUR APP HERE
 // ================================
+
+
+let O = {
+    start: function (options) {
+        console.log('===================== INDEX START =====================');
+        let el = options.el;
+
+        el.innerHTML = Markup;
+
+        return Promise.resolve({
+            stop: function () {
+                console.log('===================== INDEX STOP =====================');
+                return Promise.resolve();
+            }
+        });
+    }
+};
+
+export default O;
